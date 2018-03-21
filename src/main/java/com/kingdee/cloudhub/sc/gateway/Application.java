0e25e6c45152cf6a252e46690fa7b77e1e8d0611
jinshan_li <jinshan_li@kingdee.com>
2018-03-21 17:15:52 +0800

    初始化提交

---
+++ src/main/java/com/kingdee/cloudhub/sc/gateway/Application.java
@@ -0,0 +1,20 @@
+package com.kingdee.cloudhub.sc.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author lijinshan
+ * @date 2018/3/21 11:02
+ * @desc 启动类
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
